84224ec05015d271685bbd37821904aefcd41b6d
Estefano Mateo Proaño Ingavelez <estefano.proano@epn.edu.ec>
2024-11-27 14:46:47 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+// src/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        int resultado = suma(5, 3);
+        System.out.println("La suma de 5 y 3 es: " + resultado);
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
